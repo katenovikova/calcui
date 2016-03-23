0414639cd23494547fa6f128352711faa098961a
none23 <none23>
2016-03-23 11:47:07 +0300

    input fully working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-// Empty JS for your own code to be here
+function hideEmAll() {
+  $("#winWidth1").hide();
+  $("#winWidth2").hide();
+  $("#winWidth3").hide();
+};
+$(function(){
+  $("#winWidth1").show();
+  $("#winWidth2").hide();
+  $("#winWidth3").hide();
+
+
+  $("#win1b").on("click", function() {
+    hideEmAll();
+    $("#winWidth1").show();
+  });
+  $("#win2b").on("click", function() {
+    hideEmAll();
+    $("#winWidth2").show();
+  });
+  $("#win3b").on("click", function() {
+    hideEmAll();
+    $("#winWidth3").show();
+  });
+});
