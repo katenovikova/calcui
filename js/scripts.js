182313cd791ba49d7daeca82af8bd0af8af1c17f
none23 <none23>
2016-03-24 11:20:13 +0300

    jsons not importing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+indicesAll=[1,1,1,1];
 function hideEmAll() {
   $("#winWidth1").hide();
   $("#winWidth2").hide();
@@ -9,97 +10,63 @@ function hideEmAll() {
   $("#winHeight4").hide();
 };
 
-function calculateThePrice() {
-
-};
-
 function clearMeasurements() {
 
   $("#height1_1"  )[0].value = 0;
+  $("#width1_1"  )[0].value = 0;
+
   $("#height1_2_1")[0].value = 0;
   $("#height1_2_2")[0].value = 0;
+  $("#width1_2_1")[0].value = 0;
+  $("#width1_2_2")[0].value = 0;
+
   $("#height1_3_1")[0].value = 0;
   $("#height1_3_2")[0].value = 0;
   $("#height1_3_3")[0].value = 0;
-  $("#height1_4_1")[0].value = 0;
-  $("#height1_4_2")[0].value = 0;
-  $("#height1_4_3")[0].value = 0;
-  $("#height1_4_4")[0].value = 0;
-  $("#height1_4_5")[0].value = 0;
-  $("#width1_1"  )[0].value = 0;
-  $("#width1_2_1")[0].value = 0;
-  $("#width1_2_2")[0].value = 0;
   $("#width1_3_1")[0].value = 0;
   $("#width1_3_2")[0].value = 0;
   $("#width1_3_3")[0].value = 0;
-  $("#width1_4_1")[0].value = 0;
-  $("#width1_4_2")[0].value = 0;
-  $("#width1_4_3")[0].value = 0;
-  $("#width1_4_4")[0].value = 0;
-  $("#width1_4_5")[0].value = 0;
 
   $("#height2")[0].value = 0;
   $("#width2" )[0].value = 0;
 
   $("#height3")[0].value = 0;
   $("#width3" )[0].value = 0;
-}
-
-
-function outputPrices_1() {
-
-  var h1  = $("#height1_1"  )[0].value;
-  var w1  = $("#width1_1"  )[0].value;
-
-  var h21 = $("#height1_2_1")[0].value;
-  var h22 = $("#height1_2_2")[0].value;
-  var w21 = $("#width1_2_1")[0].value;
-  var w22 = $("#width1_2_2")[0].value;
-  var
-
-  var h31 = $("#height1_3_1")[0].value;
-  var h32 = $("#height1_3_2")[0].value;
-  var h33 = $("#height1_3_3")[0].value;
-  var w31 = $("#width1_3_1")[0].value;
-  var w32 = $("#width1_3_2")[0].value;
-  var w33 = $("#width1_3_3")[0].value;
-
-  var h41 = $("#height1_4_1")[0].value;
-  var h42 = $("#height1_4_2")[0].value;
-  var h43 = $("#height1_4_3")[0].value;
-  var h44 = $("#height1_4_4")[0].value;
-  var h45 = $("#height1_4_5")[0].value;
-  var w41 = $("#width1_4_1")[0].value;
-  var w42 = $("#width1_4_2")[0].value;
-  var w43 = $("#width1_4_3")[0].value;
-  var w44 = $("#width1_4_4")[0].value;
-  var w45 = $("#width1_4_5")[0].value;
-
-  document.getElementById("Price_1_1_1").innerHTML = 1;
-  document.getElementById("Price_1_1_2").innerHTML = 1;
-  document.getElementById("Price_1_1_3").innerHTML = 1;
-  document.getElementById("Price_1_2_1").innerHTML = 1;
-  document.getElementById("Price_1_2_2").innerHTML = 1;
-  document.getElementById("Price_1_2_3").innerHTML = 1;
-  document.getElementById("Price_1_3_1").innerHTML = 1;
-  document.getElementById("Price_1_3_2").innerHTML = 1;
-  document.getElementById("Price_1_3_3").innerHTML = 1;
-  document.getElementById("Price_1_4_1").innerHTML = 1;
-  document.getElementById("Price_1_4_2").innerHTML = 1;
-  document.getElementById("Price_1_4_3").innerHTML = 1;
-
 };
 
 
-function outputPrices_2() {
-  var h2  = $("#height2"  )[0].value;
-  var w2  = $("#width2"  )[0].value;
+
+function iPrintPrices(indicesAll) {
+  var miniPs = require('./mini.json');
+  var uniPs = require('./ini.json');
+  var litePs = require('./lite.json');
+  var luxPs = require('./lux.json');
+
+  var indexLite = $indicesAll[0];
+  var indexMini = $indicesAll[1];
+  var indexUni = $indicesAll[2];
+  var indexLux = $indicesAll[3];
+
+  var Plite = $litePs[indexLite];
+  var Pmini = $miniPs[indexMini];
+  var Puni = $uniPs[indexUni];
+  var Plux = $luxPs[indexLux];
+
+  document.getElementById("Price_1_1_1").innerHTML = Plite[0];
+  document.getElementById("Price_1_1_2").innerHTML = Plite[1];
+  document.getElementById("Price_1_1_3").innerHTML = Plite[2];
+  document.getElementById("Price_1_2_1").innerHTML = Pmini[0];
+  document.getElementById("Price_1_2_2").innerHTML = Pmini[1];
+  document.getElementById("Price_1_2_3").innerHTML = Pmini[2];
+  document.getElementById("Price_1_3_1").innerHTML = Puni[0];
+  document.getElementById("Price_1_3_2").innerHTML = Puni[1];
+  document.getElementById("Price_1_3_3").innerHTML = Puni[2];
+  document.getElementById("Price_1_4_1").innerHTML = Plux[0];
+  document.getElementById("Price_1_4_2").innerHTML = Plux[1];
+  document.getElementById("Price_1_4_3").innerHTML = Plux[2];
+
 };
 
-function outputPrices_3() {
-  var h3  = $("#height3"  )[0].value;
-  var w3  = $("#width3"  )[0].value;
-};
 
 $(function(){
   $("#winWidth1").show();
@@ -117,6 +84,7 @@ $(function(){
     clearMeasurements();
     $("#winWidth1").show();
     $("#winHeight1").show();
+
   });
 
   $("#win2b").on("click", function() {
@@ -133,24 +101,18 @@ $(function(){
     $("#winHeight3").show();
   });
 
-  $("#win4b").on("click", function() {
-    hideEmAll();
-    clearMeasurements();
-    $("#winWidth4").show();
-    $("#winHeight4").show();
-  });
 
 
 
 
   $(".measurementInputOne").change(function() {
-    outputPrices_1()
+    iPrintPrices();
   });
   $(".measurementInputTwo").change(function() {
-    outputPrices_2()
+    calculateThePrice2()
   });
   $(".measurementInputThree").change(function() {
-    outputPrices_3()
+    calculateThePrice3()
   });
 
 });
